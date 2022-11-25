2cdf4eb35734a92401bf22776d9bcf4fbe839d18
Kelve <kelvenunes123@gmail.com>
2022-11-25 11:48:20 -0300

    :new: enviando email(in progress)

---
+++ src/modelos/sendMail.js
@@ -0,0 +1,16 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "recsenhacardgame@gmail.com",
+        pass: "lfdpmsbbqlohpeza",
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
+
+module.exports = transporter;
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,10 +1,34 @@
-const express = require('express')
-
+const express = require('express');
+const transporter = require('../modelos/sendMail');
 const router = express.Router();
 
+function sendEmail(nome, idade, email, telefone) {
+    transporter.sendMail({
+        from: nome + '<recsenhacardgame@gmail.com>',
+        to: email,
+        subject: 'Agendamento' ,
+        text: "olá, me chamo " + nome + 'tenho ' + idade + 'anos de idade e meu contato é ' + telefone +' e gostaria de marcar um atendimento'
+    });
+    
+}
+
 router.get('/', async (req, res)=>{
+    res.render('../views/index')
     console.log('ólá');
-    res.render('../views/index');
+    
+
+    const nome = req.body.nomecompleto;
+    const idade = req.body.Idade;
+    const email = req.body.email;
+    const telefone = req.body.telefone;
+    
+
+    sendEmail(nome,idade,email,telefone);
+    console.log(req.body)
+    
+    
+    
 })
+
 
 module.exports = router;
